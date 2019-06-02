4547b31aef5e67cff67216b7875ab0559a86c56a
Desu <desu@pokemmo.eu>
2019-06-02 05:52:50 +0000

    Notes on how to build the native .so * Maybe works

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git clone --depth 1 https://android.googlesource.com/platform/external/bsdiff
+
+cp bsdiff_jni.cc bsdiff/
+cp com_google_archivepatcher_generator_bsdiff_wrapper_BsDiffNativePatchWriter.h bsdiff/
+
+cd bsdiff
+git clone --depth 1 https://github.com/abseil/abseil-cpp.git
+git clone --depth 1 https://github.com/google/brotli.git
+
+git apply ../make.patch
+
+make
+strip *.so
